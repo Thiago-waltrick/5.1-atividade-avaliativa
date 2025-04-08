c0f939568e94ea5a5d7c96678e2b81ffb7ef7800
Thiago W <thiago.a.waltrick@gmail.com>
2025-04-08 18:47:26 -0300

    mechi só no java para fazer a 7 e a 8

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 function calcularIMC() {
-    // entrada
+    //entrada
     var nome = document.getElementById('nome').value;
     var altura = parseFloat(document.getElementById('altura').value) / 100; 
     var peso = parseFloat(document.getElementById('peso').value);
 
-    
+  
     if (nome && altura && peso) {
         //IMC
         var imc = peso / (altura * altura);
 
         //classificação
         var classificacao = '';
-        if (imc < 18.5) {
-            classificacao = 'Abaixo do peso';
-        } else if (imc >= 18.5 && imc < 24.9) {
+
+        if (imc < 16) {
+            classificacao = 'Baixo peso muito grave';
+        } else if (imc >= 16 && imc < 16.99) {
+            classificacao = 'Baixo peso grave';
+        } else if (imc >= 17 && imc < 18.49) {
+            classificacao = 'Baixo peso';
+        } else if (imc >= 18.5 && imc < 24.99) {
             classificacao = 'Peso normal';
-        } else if (imc >= 25 && imc < 29.9) {
+        } else if (imc >= 25 && imc < 29.99) {
             classificacao = 'Sobrepeso';
-        } else {
-            classificacao = 'Obesidade';
+        } else if (imc >= 30 && imc < 34.99) {
+            classificacao = 'Obesidade grau I';
+        } else if (imc >= 35 && imc < 39.99) {
+            classificacao = 'Obesidade grau II';
+        } else if (imc >= 40) {
+            classificacao = 'Obesidade grau III';
         }
 
-        //resultados
-        document.getElementById('imc').textContent = nome + ', seu IMC é: ' + imc.toFixed(2);
-        document.getElementById('classificacao').textContent = 'Classificação: ' + classificacao;
+        
+        var mensagem = nome + ' possui índice de massa corporal igual a ' + imc.toFixed(2) + ', sendo classificado como: ' + classificacao + '.';
+
+     
+        mensagem = mensagem.toUpperCase();
+
+    
+        document.getElementById('imc').textContent = mensagem;
         document.getElementById('resultado').style.display = 'block';
     } else {
         alert('Por favor, preencha todos os campos!');
